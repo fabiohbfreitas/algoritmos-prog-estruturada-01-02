6cea9aa950b307885878ecdc6e2168e22078bf0f
fabiohbfreitas <difreitas.fabio@gmail.com>
2023-03-16 22:04:25 -0300

    add problem comment

--- exercicio01.c
+++ exercicio01.c
@@ -1,5 +1,12 @@
 #include "utils.h"
 
+// 1.Faça um programa em que o usuário digite o custo de uma determinada 
+//  mercadoria, em seguida, adiciona-se ao custo o valor do frete e despesas 
+//  eventuais (também solicitadas pelo teclado). Para concluir, o programa 
+//  pergunta qual o valor de venda e indica a percentagem de lucro da 
+//  mercadoria.
+
+
 int read_positive_integer(const char text_prompt[],const char error_msg[]);
 
 int main() {
