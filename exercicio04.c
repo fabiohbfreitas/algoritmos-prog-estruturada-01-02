f41f59c26d2332eeb7f1d2abc9b0cac65b2a0fcf
fabiohbfreitas <difreitas.fabio@gmail.com>
2023-03-16 22:41:51 -0300

    add solution to exercise 04

---
+++ exercicio04.c
@@ -0,0 +1,23 @@
+#include "utils.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+// 4. Escreva um algoritmo para ler as dimensões de um 
+// retângulo (base e altura), calcular e escrever a área do retângulo. 
+
+
+int main() {
+  const int rect_width = read_positive_integer(
+      "Digite o valor da base do retângulo: ", 
+      "Insira um valor válido para a base do retângulo."
+    );
+  const int rect_height = read_positive_integer(
+      "Digite o valor da altura do retângulo: ", 
+      "Insira um valor válido para a altura do retângulo."
+    );
+  system("clear");
+  const int rect_area = rect_height * rect_width;
+  printf("A área do retângulo é %d\n", rect_area);
+
+  return 0;
+}
