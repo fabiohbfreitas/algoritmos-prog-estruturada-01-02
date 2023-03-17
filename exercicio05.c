ec6cc8bb1b1f66e03bffd463b444108369e47626
fabiohbfreitas <difreitas.fabio@gmail.com>
2023-03-16 22:47:56 -0300

    add solution to exercise 05

---
+++ exercicio05.c
@@ -0,0 +1,31 @@
+#include "utils.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+// 5. Faça um algoritmo que leia a idade de uma pessoa expressa em anos, 
+// meses e dias e escreva a idade dessa pessoa expressa apenas em dias. 
+// Considerar ano com 365 dias e mês com 30 dias. 
+
+#define DAYS_IN_YEAR 365
+#define DAYS_IN_MONTH 30
+
+int main() {
+  const int age_years = read_positive_integer(
+    "Digite a quantidade de anos: ", 
+      "Insira uma quantidade de anos válida."
+    );
+  const int age_months = read_positive_integer(
+    "Digite a quantidade de meses: ", 
+      "Insira uma quantidade de meses válida."
+    );
+  const int age_days = read_positive_integer(
+    "Digite a quantidade de dias: ", 
+      "Insira uma quantidade de dias válida."
+    );
+
+  system("clear");
+  const int total_age_in_days = age_years * DAYS_IN_YEAR + age_months * DAYS_IN_MONTH + age_days;
+  printf("A idade em dias é %d.\n", total_age_in_days);
+
+  return 0;
+}
