ecf5066a3ba2c29387d5e5326d2f24eb32b2796b
fabiohbfreitas <difreitas.fabio@gmail.com>
2023-03-18 10:52:18 -0300

    add some validation (not ideal)

--- exercicio05.c
+++ exercicio05.c
@@ -13,16 +13,34 @@ int main() {
 
   const int age_years = read_positive_integer(
     "Digite a quantidade de anos: ", 
-      "Insira uma quantidade de anos válida."
-    );
+    "Insira uma quantidade de anos válida."
+  );
+  
+  if (age_years > 100) {
+    printf("[ERRO] A idade deve ser menor que 100.\n\n");
+    exit(EXIT_FAILURE);
+  }
+
+
   const int age_months = read_positive_integer(
     "Digite a quantidade de meses: ", 
       "Insira uma quantidade de meses válida."
     );
+  
+  if (age_months > 11) {
+    printf("[ERRO] A quantidade de meses deve ser menor que 11.\n\n");
+    exit(EXIT_FAILURE);
+  }
+  
   const int age_days = read_positive_integer(
     "Digite a quantidade de dias: ", 
       "Insira uma quantidade de dias válida."
-    );
+  );
+
+  if (age_days > 29) {
+    printf("[ERRO] A quantidade de dias deve ser menor que 29.\n\n");
+    exit(EXIT_FAILURE);
+  }
 
   clear_terminal();
   const int total_age_in_days = age_years * DAYS_IN_YEAR + age_months * DAYS_IN_MONTH + age_days;
