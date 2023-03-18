e82b87a0f31275a50d7ec16e306a475c63084628
fabiohbfreitas <difreitas.fabio@gmail.com>
2023-03-17 22:36:04 -0300

    add solution to exercise 10

---
+++ exercicio10.c
@@ -0,0 +1,24 @@
+#include "utils.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+  int a_value = read_integer("Digite o valor do coeficiente a: ", 1, "O valor de a não pode ser zero.");
+  int b_value = read_integer("Digite o valor do coeficiente b: ", 0, "");
+  int c_value = read_integer("Digite o valor do coeficiente c: ", 0, "");
+  system("clear");
+  double discriminant = (b_value * b_value) - (4 * a_value * c_value);
+  float result[2];
+  if (discriminant < 0) {
+    printf("A equação não possui soluções no conjunto dos números reais.\n");
+  } else if (discriminant == 0) {
+    result[0] = ((float) (-1 * b_value)) / (2 * a_value);
+    printf("A equação possui apenas uma solução: %.2f\n", result[0]);
+  } else {
+    result[0] = ((-1 * b_value) + (float) sqrt(discriminant)) / (2 * a_value);
+    result[1] = ((-1 * b_value) - (float) sqrt(discriminant)) / (2 * a_value);
+    printf("A equação possui duas soluções: %.2f e %.2f\n", result[0], result[1]);
+  }
+
+  return EXIT_SUCCESS;
+}
--- utils.h
+++ utils.h
@@ -1,5 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
+
 #ifndef UTILS__H
 #define UTILS__H
 
@@ -20,4 +22,21 @@ int read_positive_integer(const char text_prompt[],const char error_msg[])
 
     return readed_value;
 }
+
+int read_integer(const char text_prompt[], int should_not_be_zero, const char err_msg[])
+{
+    int readed_value = -1;
+    do {
+        system("clear");
+        if (readed_value == 0) {
+            printf("%s\n\n", err_msg);
+        }
+        printf("%s", text_prompt);
+        scanf("%d", &readed_value);
+    }
+    while (should_not_be_zero && (readed_value == 0));
+
+    return readed_value;
+}
+
 #endif
