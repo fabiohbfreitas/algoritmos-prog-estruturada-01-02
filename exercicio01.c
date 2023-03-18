b1605c58a67a666de52371e7b3639f1a0bb18ee4
fabiohbfreitas <difreitas.fabio@gmail.com>
2023-03-18 09:28:18 -0300

    change variable to const

--- exercicio01.c
+++ exercicio01.c
@@ -11,29 +11,29 @@
 int main() {
     setlocale(LC_ALL, "Portuguese");
     
-    int product_cost = read_positive_float(
+    const int product_cost = read_positive_float(
         "Digite o custo do produto: ", 
         "O custo deve ser um preço válido.\n"
         );
 
-    int shipping_cost = read_positive_float(
+    const int shipping_cost = read_positive_float(
         "Digite o custo do frete: ", 
         "O custo deve ser um preço válido."
         );
 
-    int other_cost = read_positive_float(
+    const int other_cost = read_positive_float(
         "Digite o preço de outros custos: ", 
         "O custo deve ser um preço válido."
     );
 
-    int selling_price = read_positive_float(
+    const int selling_price = read_positive_float(
         "Digite o preço de venda: ", 
         "Digite um preço válido."
     );
 
-    float total_cost = product_cost + shipping_cost + other_cost;
-    float profit = selling_price - total_cost;
-    float profit_margin = ((float) profit) / ((float) selling_price);
+    const float total_cost = product_cost + shipping_cost + other_cost;
+    const float profit = selling_price - total_cost;
+    const float profit_margin = ((float) profit) / ((float) selling_price);
     
     clear_terminal();
     printf("Lucro: R$%.2f | Margen de Lucro: %.2f%%\n", profit, profit_margin * 100);
--- exercicio02.c
+++ exercicio02.c
@@ -10,12 +10,12 @@
 int main() {
   setlocale(LC_ALL, "Portuguese");
   
-  float circle_radius = read_positive_float(
+  const float circle_radius = read_positive_float(
       "Digite o raio do círculo: ", 
       "Digite um valor válido para o raio."
     );
   clear_terminal();
-  float circle_area =  PI * (float) pow(circle_radius, 2.0);
+  const float circle_area =  PI * (float) pow(circle_radius, 2.0);
   printf("A área do círculo é: %.2f\n", circle_area);
 
   return 0;
